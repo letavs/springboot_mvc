6e25c9b872af09f793d7c04d89bcf7d33661bd33
Leandro Tavares <letavs@gmail.com>
2020-08-05 23:49:37 -0300

    Artigo: Entendendo a comunicação view/controller via GET

---
+++ src/main/java/br/com/devmedia/model/Caminhada.java
@@ -0,0 +1,24 @@
+package br.com.devmedia.model;
+
+import java.time.LocalTime;
+
+public class Caminhada {
+	private LocalTime tempoMaximo;
+	private LocalTime tempoMinimo;
+
+	public LocalTime getTempoMaximo() {
+		return tempoMaximo;
+	}
+
+	public void setTempoMaximo(final LocalTime tempoMaximo) {
+		this.tempoMaximo = tempoMaximo;
+	}
+
+	public LocalTime getTempoMinimo() {
+		return tempoMinimo;
+	}
+
+	public void setTempoMinimo(final LocalTime tempoMinimo) {
+		this.tempoMinimo = tempoMinimo;
+	}
+}
